110a897f49f585386b953c4be07638d7b6e5ff80
DWS2809 <k6227488@gmail.com>
2025-12-17 09:41:00 +0000

    Add python_introduction simple_interest.py and it's task

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+#simple_interest.py
+
+principal = 1000 # principal amount
+rate = 0.05 # annual interest rate in percentage
+time = 3 # time in years
+
+# Calculate simple interest
+simple_interest = principal * rate * time
+
+print(f"The simple interest is: {simple_interest}")
+
