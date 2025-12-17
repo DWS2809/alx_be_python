0049abae02578673fbc28519f370c7f864a50f9b
DWS2809 <k6227488@gmail.com>
2025-12-17 09:50:56 +0000

    Add python_introduction/rectangle_area.py task

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,7 @@
+# Calculate the area of a rectangle
+Length = 5 #Length of the rectangle
+Width = 3 #Width of the rectange
+
+# Calculate area
+area = Length * Width
+print(f"The area of the rectangle is: {area}")
