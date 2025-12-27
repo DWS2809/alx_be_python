1930cda6df3c50905173219c4a278ba45eb5d626
DWS2809 <k6227488@gmail.com>
2025-12-27 00:02:43 +0000

    Add python_introduction/practice.py

---
+++ python_introduction/practice.py
@@ -0,0 +1,8 @@
+def check_even_or_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is Even")
+    else:
+        print(f"{number} is Odd")
+# Example usage
+check_even_or_odd(10)
+check_even_or_odd(7)
