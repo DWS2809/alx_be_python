fb993073ccc142cc540e7f6fc54897aa76b19bed
DWS2809 <k6227488@gmail.com>
2025-12-27 00:17:09 +0000

    Add python_introduction/calculator.py and python_introduction/main.py

---
+++ python_introduction/main.py
@@ -0,0 +1,9 @@
+# main.py
+import calculator
+num1 =20
+num2 = 30
+
+print("Addition:", calculator.add(num1, num2))
+print("Subtraction:", calculator.subtract(num1, num2))
+print("Multiplication:", calculator.multiply(num1, num2))
+print("Division:", calculator.divide(num1, num2))
